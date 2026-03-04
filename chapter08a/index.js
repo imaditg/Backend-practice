31f096b695d96a3fe98f3d423f8e18e15e4cb1cc
imaditg <imaditg@outlook.com>
2026-03-04 20:12:19 +0530

    chapter08a

---
+++ chapter08a/index.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+const {engine} = require('express-handlebars');
+
+app.engine('handlebars',engine({
+    defaultLayout:'main'
+}))
+
+app.set('view engine','handlebars')
+
+app.get('/',(req,res)=>{
+    res.render('home')
+})
+
+app.get('/form',(req,res)=>{
+    res.render('forms')
+})
+
+app.get('/form-submit',(req,res)=>{
+    console.log(req.query.name)
+    console.log(req.query.email)
+    res.redirect(303,'/thanks')
+})
+
+app.get('/thanks',(req,res)=>{
+    res.render('thanks')
+})
+
+app.use((req,res)=>{
+    res.render('404');
+})
+
+app.use((err,req,res,next)=>{
+    res.render('500')
+})
+
+app.listen(port,()=>{
+    console.log(`server started on https://localhost:${port}`);
+})
